690d51e101f038b2c3552bacfe9ef2f3fa5d5f9b
helium-service <helium-service@chartboost.com>
2026-07-22 09:51:26 -0700

    [AUTO-GENERATED] Synchronizing changes from private repository

--- Runtime/InMobi/InMobiAdapter.cs
+++ Runtime/InMobi/InMobiAdapter.cs
@@ -12,7 +12,7 @@ namespace Chartboost.Mediation.InMobi
         /// <summary>
         /// The partner adapter Unity version.
         /// </summary>
-        public const string AdapterUnityVersion = "5.2.6";
+        public const string AdapterUnityVersion = "5.2.7";
         
         /// <inheritdoc cref="IPartnerAdapterConfiguration.AdapterNativeVersion"/>
         public static string AdapterNativeVersion => Instance.AdapterNativeVersion;
